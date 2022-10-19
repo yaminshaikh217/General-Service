cb9ba91815ee71e1d840b8308397c1ad10839a15
Yamin Shaikh <yaminshaikh.ys47@gmail.com>
2022-10-19 21:00:15 +0530

    Nav finished

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
 
 const Navbar = () => {
+
+  const [active, setactive] = useState(false)
+  const handleActive = () => { setactive(!active) }
+
   return (
     <>
+      <nav className="navbar">
+        <header className="container header">
+          <div className="logo">PEGEON</div>
+          <ul className={`navLinks ${active ? "active" : ""}`}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Experts</li>
+            <li>Pricing</li>
+            <li>News</li>
+            <li>Contact</li>
+          </ul>
+          <div className="hamburgerMenu" onClick={handleActive}>djdfhdfjdskjfhkfjj</div>
+        </header>
+      </nav>
     </>
   )
 }
